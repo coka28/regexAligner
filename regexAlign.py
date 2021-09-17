b35b2c869b2051e72e62e67b474d4b1746aee9ae
coka28 <70796761+coka28@users.noreply.github.com>
2021-09-17 22:04:34 +0200

    Update regexAlign.py

--- regexAlign.py
+++ regexAlign.py
@@ -5,25 +5,33 @@
 
 import re
 
-r = r'convolution[\W:-]*is not a.* crime'
-s = r'convolution: --\tis not a fucking crime, but worse'
+r = r'when [Ii] tasked my mom[, iI]* didnt flinch to fuck .. ass'
+s = 'when i asked my, mom, I didn\'t even flinch to fuck an ass'
 
 def regexAlign(regex,text):
+    class Flex:
+        def __init__(self,match,offset=0):
+            self.g = match.group()
+            self.s = match.span()[0]+offset,match.span()[1]+offset
+        def group(self):
+            return self.g
+        def span(self):
+            return self.s
+    
     i = 0
     flex = []
     while re.search(r'\[[^\]]*\**\+*\]\+*\**',regex[i:]):
-        tmp = re.search(r'\[[^\]]*\**\+*\]\+*\**',regex[i:])
-        if regex[i+tmp.span()[0]-1]=='\\':
+        tmp = Flex(re.search(r'\[[^\]]*\**\+*\]\+*\**',regex[i:]),i)
+        if regex[tmp.span()[0]-1]=='\\':
             i = tmp.span()[1]
             continue
         flex.append(tmp)
         i = flex[-1].span()[1]
     
-    
     i = 0
     while re.search(r'\.\+*\**',regex[i:]):
-        tmp = re.search(r'\.\+*\**',regex[i:])
-        if regex[i+tmp.span()[0]-1]=='\\':
+        tmp = Flex(re.search(r'\.\+*\**',regex[i:]),i)
+        if regex[tmp.span()[0]-1]=='\\':
             i = tmp.span()[1]
             continue
         flex.append(tmp)
@@ -53,35 +61,82 @@ def regexAlign(regex,text):
     text = '\x00' + text
 
     ################################################
+    global scores,pointers
 
     s1,s2 = regtext,text
 
     scores   = [[0 for i in s2] for k in s1]
     pointers = [[None for i in s2] for k in s1]
+    flex     = [f.group() for f in flex]
+    R,C,M = 0,0,0
 
     for r in range(1,len(s1)):
         if r in flexpos:
             i  = flexpos.index(r)
             rx = flex[i]
-            if flextype[i] == 0:
+            if flextype[i] == 1:
                 for c in range(len(s2)):
-                
+                    mt = 0 if re.search(rx,s2[c]) else -1
+                    s  = (scores[r-1][c-1]+mt,
+                          scores[r-1][c]-1,
+                          scores[r][c-1]-1)
+                    ms = max(s)
+                    pt = s.index(ms)
+                    if ms>0:
+                        scores[r][c]   = ms
+                        pointers[r][c] = pt
+                        if M<=ms: M,R,C = ms,r,c
+            elif flextype[i] == 2:
+                rx = rx[:-1]
+                for c in range(len(s2)):
+                    mt = 0 if re.search(rx,s2[c]) else -1
+                    s  = (scores[r-1][c-1]+mt,
+                          scores[r-1][c]-1,
+                          scores[r][c-1]+mt)
+                    ms = max(s)
+                    pt = s.index(ms)
+                    if ms>0:
+                        scores[r][c]   = ms
+                        pointers[r][c] = pt
+                        if M<=ms: M,R,C = ms,r,c
+            else:
+                rx = rx[:-1]
+                for c in range(len(s2)):
+                    mt = 0 if re.search(rx,s2[c]) else -1
+                    s  = (scores[r-1][c-1]+mt,
+                          scores[r-1][c]+mt,
+                          scores[r][c-1]+mt)
+                    ms = max(s)
+                    pt = s.index(ms)
+                    if ms>0:
+                        scores[r][c]   = ms
+                        pointers[r][c] = pt
+                        if M<=ms: M,R,C = ms,r,c
+        else:
+            for c in range(1,len(s2)):
+                s  = (scores[r-1][c-1]+(1 if regtext[r]==text[c] else -1),
+                      scores[r-1][c]-1,
+                      scores[r][c-1]-1)
+                ms = max(s)
+                pt = s.index(ms)
+                if ms>0:
+                    scores[r][c]   = ms
+                    pointers[r][c] = pt
+                    if M<=ms: M,R,C = ms,r,c
 
-                    
-            continue
-        for c in range(1,len(s2)):
-            s  = (scores[r-1][c-1]+(1 if regtext[r]==text[c] else -1),
-                  scores[r-1][c]-1,
-                  scores[r][c-1]-1)
-            ms = max(s)
-            pt = s.index(ms)
-            if ms>0:
-                scores[r][c]   = ms
-                pointers[r][c] = pt
+    t1,t2 = '',''
 
-
-
-
-
-
-
+    while pointers[R][C] is not None:
+        pt = pointers[R][C]
+        if pt==0:
+            t1,t2 = s1[R]+t1,s2[C]+t2
+            R -= 1
+            C -= 1
+        elif pt==1:
+            t1,t2 = s1[R]+t1,'_'+t2
+            R -= 1
+        elif pt==2:
+            t1,t2 = '*'+t1,s2[C]+t2
+            C -= 1
+    
+    return t1,t2
